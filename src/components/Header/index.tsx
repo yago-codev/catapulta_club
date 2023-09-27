849a15ecb53f5a73ae0e70c6b193aa7077db7c80
yago-codev <yagomilano92@gmail.com>
2023-09-27 00:00:55 -0300

    add styled-components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,16 @@
 import React, {FC} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
 import {Props} from './types';
 
+import * as S from './styles';
+
 const Header: FC<Props> = props => {
   return (
-    <View>
-      <Text style={style.Title}>{props.title}</Text>
-      <Text style={style.Title}>{props.name}</Text>
-    </View>
+    <S.Container>
+      <S.Title>{props.title}</S.Title>
+      <S.Name>{props.name}</S.Name>
+    </S.Container>
   );
 };
-
-const style = StyleSheet.create({
-  Title: {
-    color: 'black',
-    fontSize: 30,
-  },
-  Subtitle: {
-    color: 'black',
-    fontSize: 15,
-  },
-});
 
 export default Header;
